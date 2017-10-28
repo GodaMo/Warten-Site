e84cdb1d1ae76ffe7b07ee0f227243e6538e146d
GodaMo <godamo.mailpro@yahoo.com>
2017-10-28 18:46:52 +0200

    Test Unitaire sur la connexion

--- controller/config/daoController.php
+++ controller/config/daoController.php
@@ -4,23 +4,5 @@ include_once('../model/modelAll.php');
 include_once('../dao/DAOAll.php');
 
 
-/*
-
-$Examplearticles = new articlesDAO();
-$Examplecommentaire = new commentaireDAO();
-$Exampledroit = new droitDAO();
-$Examplelien = new lienDAO();
-$Examplemessage = new messageDAO();
-$Exampleparametre = new parametreDAO();
-$Exampleprofil = new profilDAO();
-$Exampleprofiljoueur = new profiljoueurDAO();
-$Exampletraduction = new traductionDAO();
-$Exampleuser = new userDAO();
-
-
-
-*/
-
-
 
 ?>
--- controller/test/AllTestDAO.php
+++ controller/test/AllTestDAO.php
@@ -2,29 +2,17 @@
 
 include_once('../model/modelAll.php');
 include_once('../dao/DAOAll.php');
-/*
-$Examplearticles = new articlesDAO();
-$Examplecommentaire = new commentaireDAO();
-$Exampledroit = new droitDAO();
-$Examplelien = new lienDAO();
-$Examplemessage = new messageDAO();
-$Exampleparametre = new parametreDAO();
-$Exampleprofil = new profilDAO();
-$Exampleprofiljoueur = new profiljoueurDAO();
-$Exampletraduction = new traductionDAO();*/
-$Exampleuser = new userDAO();
+session_start();
+$pseudoPOST = 'warten';
+$userDao = new userDAO();
+$req = $userDao->get_user();
+for ($i=0; $i <count($req); $i++) {
+	if($req[$i]->get_user_pseudo() == $pseudoPOST){
+		echo("vous êtes connecter : " . $pseudoPOST);	
+	}else{
 
-/*Views*/
-/*print_r($Examplearticles->get_articles());
-print_r($Examplecommentaire->get_commentaire());
-print_r($Exampledroit->get_droit());
-print_r($Examplelien->get_lien());
-print_r($Examplemessage->get_message());
-print_r($Exampleparametre->get_parametre());
-print_r($Exampleprofil->get_profil());
-print_r($Exampleprofiljoueur->get_profiljoueur());
-print_r($Exampletraduction->get_traduction());*/
-//print_r($Exampleuser->update_articles($articles));
- print_r($Exampleuser->get_user());
+	}
+}
+
 
 ?>
